d841f5963d50e33e9dd1b4e8c84fa7672d65d7de
wilsdefots <fotsing.wilson88@gmail.com>
2025-02-07 13:37:04 -0500

    labo terminé

--- TesteurXBots.cpp
+++ TesteurXBots.cpp
@@ -10,21 +10,21 @@ TesteurXBots::TesteurXBots(X212* x212, G990* g990)
 {
 	this->x212 = x212;
 	this->g990 = g990;
-	Compteur::ajouterConstructeur; 
+	Compteur::ajouterConstructeur(); 
 }
 
 TesteurXBots::~TesteurXBots()
 {
 	delete(x212);
 	delete(g990);
-	Compteur::ajouterDestructeur;
+	Compteur::ajouterDestructeur();
 }
 
 TesteurXBots::TesteurXBots(const TesteurXBots& testeurXBots)
 {
 	this->x212 = new X212(*testeurXBots.x212);
 	this->g990 = new G990(*testeurXBots.g990);
-	Compteur::ajouterConstructeurCopie;
+	Compteur::ajouterConstructeurCopie();
 }
 
 
